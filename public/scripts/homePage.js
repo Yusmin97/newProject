5768cde4376a0b96dfabaa24e249b06372b10700
Yusmin97 <tmdamsi97@gmail.com>
2023-11-27 20:31:47 +0900

    동적 생성한 div태그에 클래스명 지정

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -1,6 +1,7 @@
 // homePage.js
 export const createHomePage = () => {
   const homePage = document.createElement('div');
+  homePage.setAttribute("class", "homePage");
   homePage.innerHTML = `
     <p>첫 번째 페이지의 내용이 여기에 들어갑니다.</p>
     <button onclick="navigateToNextPage()">다음 페이지로 이동</button>
