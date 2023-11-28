c2f36ff033f99e3e82d8c64da949129d12c1ce21
Yusmin97 <tmdamsi97@gmail.com>
2023-11-28 15:41:47 +0900

    header.js 파일명 변경

--- public/scripts/header.js
+++
@@ -1,7 +0,0 @@
-export const createHeader = () => {
-  const header = document.createElement('header');
-  header.innerHTML = `
-    <button class="hamburger-menu">☰</button>
-  `;
-  return header;
-};
