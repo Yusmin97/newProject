de92d6bbf42300ce2bf5ec0d25663d867f414784
Yusmin97 <tmdamsi97@gmail.com>
2023-11-30 17:55:56 +0900

    정적 파일 서빙 설정 때 경로 문제점 발견

--- routes/route.js
+++ routes/route.js
@@ -4,11 +4,13 @@ const path = require('path');
 const fs = require('fs');
 
 // 정적 파일 서빙 설정
-router.use('/public', express.static(path.join(__dirname, '..', 'public')));
+router.use(express.static(path.join(__dirname, '..', 'public')));
+// 정적 파일 서빙 설저을 할 때 '/public'을 사용하닌 경로 설정에 문제가 생겼다.
+// router.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 // 라우트 설정
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = router;
