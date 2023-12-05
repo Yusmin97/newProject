9ea0660c9f87d83ab4ccc7853204f15e756faa6d
Yusmin97 <tmdamsi97@gmail.com>
2023-12-05 21:19:55 +0900

    주석 내용 변경

--- public/scripts/container.js
+++ public/scripts/container.js
@@ -1,10 +1,9 @@
-// homePage.js
+// container.js
 export const createContainer = () => {
   const container = document.createElement('div');
   container.setAttribute("id", "container");
   container.innerHTML = `
     <p>첫 번째 페이지의 내용이 여기에 들어갑니다.</p>
-    <input type="button" name="valueBtn">다음 페이지로 이동
   `;
   return container;
 };
