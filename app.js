a29e0a4cd6f2e1c203fede897d524fc4b4a1b19e
Yusmin97 <tmdamsi97@gmail.com>
2023-12-08 12:50:26 +0900

    cors 추가

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mainRoute = require('./routes/route');
 const petInfoRoute = require('./routes/petInfoRoute');
+const cors = require('cors');
 const app = express();
 const port = 9797;
 
 // 라우트를 관리하는 파일에서 모듈화 한 라우트 사용하기
+app.use(cors());
 app.use('/', mainRoute);
 app.use('/', petInfoRoute);
 
