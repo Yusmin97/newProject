7c3e6567eec196702f17da503bc6248bdb707c3f
Yusmin97 <tmdamsi97@gmail.com>
2023-12-08 12:08:33 +0900

    메인 서버에 petInfoRoute 추가

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route');
+const mainRoute = require('./routes/route');
+const petInfoRoute = require('./routes/petInfoRoute');
 const app = express();
 const port = 9797;
 
 // 라우트를 관리하는 파일에서 모듈화 한 라우트 사용하기
-app.use('/', route);
+app.use('/', mainRoute);
+app.use('/', petInfoRoute);
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중 입니다.`)
