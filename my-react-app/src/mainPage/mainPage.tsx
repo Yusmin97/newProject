ce65105ffd23bce757ed115f0a7b5054ef141a67
Yusmin97 <tmdamsi97@gmail.com>
2023-12-18 23:07:26 +0900

    mainPage 컴포넌트 메인페이지에 작성

---
+++ my-react-app/src/mainPage/mainPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hamburger from "./hamburger";
+import Container from "./container";
+import Footer from "./footer";
+
+function MainPage() {
+  return (
+    <>
+    <Hamburger />
+    <Container />
+    <Footer />
+    </>
+  )
+}
+
+export default MainPage;
